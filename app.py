10e315d1d348c5204a823de8b698c3ab27cf264c
Angelo Stefani <40657108+angelostefani78@users.noreply.github.com>
2024-05-31 10:00:33 +0200

    update

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+#14/05/2024 - angelo.stefani@enea.it
+#RESTful service in Python to implement a keepalive mechanism.
+#Flask application with a single /keepalive route. When this route is invoked via a GET request, the keep_alive() function executes and returns a "Service reachable" response with an HTTP status code of 200, indicating that the service is active and reachable.
+#To start the service, just run this Python script. Make sure you have Flask installed in your Python environment (pip install Flask). Once the service is launched, it will be available at http://localhost:5000/keepalive.
+#You can test the service by making a GET request to http://localhost:5000/keepalive using tools like cURL, Postman, or even a simple web browser. You will see that you will receive the response "Service reachable".
+
 from flask import Flask
 
 # Create a Flask application
